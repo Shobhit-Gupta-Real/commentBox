9078ff0731b3bd452c1955a5b7d574c7aa08c0be
shobhit.gupta@herovired.com-at-725799283397 <shobhit.gupta@herovired.com>
2024-11-23 13:49:28 +0530

    added userManager

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import {server as WebSocketServer} from 'websocket'
+import { server as WebSocketServer } from 'websocket'
 import http from 'http';
 
-var server = http.createServer(function(request: any, response: any) {
+const server = http.createServer(function (request: any, response: any) {
     console.log((new Date()) + ' Received request for ' + request.url);
     response.writeHead(404);
     response.end();
 });
-server.listen(8080, function() {
+server.listen(8080, function () {
     console.log((new Date()) + ' Server is listening on port 8080');
 });
 
@@ -21,31 +21,29 @@ const wsServer = new WebSocketServer({
 });
 
 function originIsAllowed(origin: string) {
-  // put logic here to detect whether the specified origin is allowed.
-  return true;
+    // put logic here to detect whether the specified origin is allowed.
+    return true;
 }
 
-wsServer.on('request', function(request) {
+wsServer.on('request', function (request) {
     if (!originIsAllowed(request.origin)) {
-      // Make sure we only accept requests from an allowed origin
-      request.reject();
-      console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
-      return;
+        // Make sure we only accept requests from an allowed origin
+        request.reject();
+        console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
+        return;
     }
-    
+
     var connection = request.accept('echo-protocol', request.origin);
     console.log((new Date()) + ' Connection accepted.');
-    connection.on('message', function(message) {
+    connection.on('message', function (message) {
+        //Add rate limmting logic here
         if (message.type === 'utf8') {
             console.log('Received Message: ' + message.utf8Data);
             connection.sendUTF(message.utf8Data);
         }
-        else if (message.type === 'binary') {
-            console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
-            connection.sendBytes(message.binaryData);
-        }
+
     });
-    connection.on('close', function(reasonCode, description) {
+    connection.on('close', function (reasonCode, description) {
         console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
     });
 });
